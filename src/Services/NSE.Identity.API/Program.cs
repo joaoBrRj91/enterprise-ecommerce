ba8ca26faf0aac4ba24c9d58a4e6a04072928b9c
joaoBrRj91 <joao1991algoritmo@gmail.com>
2025-05-29 20:46:24 -0300

    Remove unsed

--- src/Services/NSE.Identity.API/Program.cs
+++ src/Services/NSE.Identity.API/Program.cs
@@ -1,15 +1,4 @@
-using Carter;
-using FluentValidation;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using NSE.Identity.API.Configurations;
-using NSE.Identity.API.Data;
-using NSE.Identity.API.Endpoints;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
