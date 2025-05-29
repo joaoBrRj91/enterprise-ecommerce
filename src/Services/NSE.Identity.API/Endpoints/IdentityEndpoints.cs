d04b71f241e1047b3c747d8101e8f47f427fc945
joaoBrRj91 <joao1991algoritmo@gmail.com>
2025-05-29 20:46:57 -0300

    Add auth interface in constructor

--- src/Services/NSE.Identity.API/Endpoints/IdentityEndpoints.cs
+++ src/Services/NSE.Identity.API/Endpoints/IdentityEndpoints.cs
@@ -2,10 +2,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NSE.Identity.API.Models;
+using NSE.Identity.API.Providers;
 
 namespace NSE.Identity.API.Endpoints;
 
-public class IdentityEndpoints : ICarterModule
+public class IdentityEndpoints(IAuthProvider authProvider) : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
@@ -15,12 +16,13 @@ public class IdentityEndpoints : ICarterModule
             .PasswordSignInAsync(userLogin.Email, userLogin.Password, isPersistent: false, lockoutOnFailure: true);
 
             if (identityResult.Succeeded)
-                return Results.Created();
+                return Results.Ok(await authProvider.GenerateTokenAsync(userLogin.Email));
 
             return Results.BadRequest();
 
         })
         .WithName("GetIdentityUser");
+
 
         app.MapPost("/new-account", async ([FromBody] UserRegister userRegister, UserManager<IdentityUser> userManager) =>
         {
@@ -34,7 +36,7 @@ public class IdentityEndpoints : ICarterModule
             var identityResult = await userManager.CreateAsync(identityUser, userRegister.Password);
 
             if (identityResult.Succeeded)
-                return Results.Created();
+                return Results.Created(string.Empty, await authProvider.GenerateTokenAsync(userRegister.Email));
 
             return Results.BadRequest();
 
