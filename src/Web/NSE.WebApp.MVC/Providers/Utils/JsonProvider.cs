92485079a2418823e48ae26b0f360964750a799c
joaoBrRj91 <joao1991algoritmo@gmail.com>
2025-07-23 13:01:09 -0300

    Add custom options to deserialize json

--- src/Web/NSE.WebApp.MVC/Providers/Utils/JsonProvider.cs
+++ src/Web/NSE.WebApp.MVC/Providers/Utils/JsonProvider.cs
@@ -4,12 +4,21 @@ namespace NSE.WebApp.MVC.Providers.Utils
 {
     public class JsonProvider : IJsonProvider
     {
+        private readonly JsonSerializerOptions _serializerOptions;
+        public JsonProvider()
+        {
+            _serializerOptions = new()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+        }
+
         public async Task<TResponse> DeserializeResponse<TResponse>(HttpResponseMessage httpResponse, bool thowingExceptionBadRequest = true) 
             where TResponse : class
         {
             if (httpResponse.IsSuccessStatusCode)
             {
-                return JsonSerializer.Deserialize<TResponse>(await httpResponse.Content.ReadAsStringAsync())!;
+                return JsonSerializer.Deserialize<TResponse>(await httpResponse.Content.ReadAsStringAsync(), _serializerOptions)!;
             }
 
             if (thowingExceptionBadRequest)
