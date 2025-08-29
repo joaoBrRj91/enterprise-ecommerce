987823cdd6a41727985e4741dba613d6b55c416f
joaoBrRj91 <joao1991algoritmo@gmail.com>
2025-08-29 16:18:48 -0300

    Create base structure Catalog API and the base Entity in Shared

---
+++ src/Services/NSE.Catalog.API/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using NSE.Shared.DomainObjects;
+
+namespace NSE.Catalog.API.Models;
+
+public class Product : Entity
+{
+    public string Name { get; set; }
+    public string Descricao { get; set; }
+    public bool IsACtive { get; set; }
+    public decimal Price { get; set; }
+    public DateTime CreateDate { get; set; }
+    public string Image { get; set; }
+    public int Quantity { get; set; }
+
+}
---
+++ src/Services/NSE.Catalog.API/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/weatherforecast", () => new StringContent("Hello Catalog API"))
+   .WithName("ValidationStartApp")
+   .WithOpenApi();
+
+app.Run();
